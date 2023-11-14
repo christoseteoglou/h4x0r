3fa3353cf365318e1dea2d10414aaf386db95d8a
christoseteoglou <itsmeglue@gmail.com>
2023-11-14 09:51:27 +0100

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Christos Eteoglou on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
